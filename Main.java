3694a9371d0c8e841a8dd54e9f349922116cd013
Dh-Van <dhvan17shah@gmail.com>
2021-06-30 13:01:20 +0000

    Added numberpyramid

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main{
+    public static void main(String[] args) {
+        int outerCounter = 1;
+        int innerCounter = 0;
+        int displayNumber = 1;
+
+        
+    }
+}
---
+++ NuberPyramidSoluition.java
@@ -0,0 +1,26 @@
+/**
+ * Nuber Pyramid Soluition (no for loops)
+ */
+public class NuberPyramidSoluition {
+    
+
+
+    public static void main(String[] args) {
+        int outerCounter = 1;
+        int innerCounter = 0;
+        int displayNumber = 1;
+
+        while(outerCounter <= 9){
+            while(innerCounter < displayNumber){
+                System.out.print(displayNumber);
+                innerCounter = innerCounter + 1;
+            }
+            
+            System.out.println("");
+            innerCounter = 0;
+            outerCounter = outerCounter + 1;
+            displayNumber = displayNumber + 1;
+        }
+    }
+    
+}
